55a14e12418373182e4a6c0981d38e0f118ec53e
Dave Feucht <poetas@gmail.com>
2018-04-03 11:12:34 +0200

    Added comment block headers to describe what each challenge was about

--- Chapter2/loop_a_triangle.js
+++ Chapter2/loop_a_triangle.js
@@ -1,3 +1,19 @@
+/*
+
+Chapter 2: Looping A Triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
 let output_string = "#";
 
 while(output_string.length < 7) {
