ce4b080974df8172d3ddf6fd7472cc0d5e4bfef7
Dave Feucht <poetas@gmail.com>
2018-04-05 14:49:20 +0200

    Added dominant_writing_direction exercise and scripts input data from Chapter 5

---
+++ Chapter5/dominant_writing_direction.js
@@ -0,0 +1,92 @@
+/*
+
+Chapter 5: Dominant Writing Direction
+
+Write a function that computes the dominant writing direction in a string of text. Remember that 
+each script object has a direction property that can be "ltr" (left-to-right), "rtl" (right-to-left), 
+or "ttb" (top-to-bottom).
+
+The dominant direction is the direction of a majority of the characters that have a script associated 
+with them. The characterScript and countBy functions defined earlier in the chapter are probably useful 
+here.
+
+function dominantDirection(text) {
+  // Your code here.
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
+
+*/
+
+//Include the data on the different writing scripts
+let SCRIPTS = require("./scripts.js");
+
+//Functions defined in Chapter 5
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name === name);
+    if (known === -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+//Exercise code below
+let findScriptByName = function(name) {
+  let script_match = {};
+
+  for(let script of SCRIPTS) {
+    if(script.name === name) {
+      script_match = script;
+      break;
+    }
+  } 
+
+  return(script_match);   
+};
+
+let findHighestCount = function(list) {
+  let high_count = {name: undefined, count: 0}; 
+
+  for(let i = 0; i < list.length; i++) {
+    if(list[i].count > high_count.count) {
+      high_count.name = list[i].name;
+      high_count.count = list[i].count;
+    } 
+  } 
+  
+  return high_count;
+};
+
+let dominantDirection = function(string) {
+  let scripts = [];
+
+  scripts = countBy(string, character => {
+    let script = characterScript(character.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name !== "none");
+
+  return findScriptByName(findHighestCount(scripts).name).direction;
+};
+
+console.log(dominantDirection("Hello!"));
+console.log(dominantDirection("Hey, مساء الخير"));
