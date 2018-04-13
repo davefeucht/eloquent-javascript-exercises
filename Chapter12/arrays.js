c20a4a636941fd7c1eb48908d04e5f6ec9448110
Dave Feucht <poetas@gmail.com>
2018-04-13 15:34:25 +0200

    Added arrays exercise from Chapter 12

---
+++ Chapter12/arrays.js
@@ -0,0 +1,54 @@
+/*
+
+Chapter 12: Arrays
+
+Arrays
+
+Add support for arrays to Egg by adding the following three functions to the top scope: 
+array(...values) to construct an array containing the argument values, length(array) to 
+get an array’s length, and element(array, n) to fetch the nth element from an array.
+
+// Modify these definitions...
+
+topScope.array = "...";
+
+topScope.length = "...";
+
+topScope.element = "...";
+
+run(`
+do(define(sum, fun(array,
+     do(define(i, 0),
+        define(sum, 0),
+        while(<(i, length(array)),
+          do(define(sum, +(sum, element(array, i))),
+             define(i, +(i, 1)))),
+        sum))),
+   print(sum(array(1, 2, 3))))
+`);
+// → 6
+
+*/
+
+topScope.array = (...values) => {
+  return values; 
+};
+
+topScope.length = array => {
+  return array.length; 
+};
+
+topScope.element = (array, index) => {
+  return array[index]; 
+};
+
+run(`
+do(define(sum, fun(array,
+     do(define(i, 0),
+        define(sum, 0),
+        while(<(i, length(array)),
+          do(define(sum, +(sum, element(array, i))),
+             define(i, +(i, 1)))),
+        sum))),
+   print(sum(array(1, 2, 3))))
+`);
